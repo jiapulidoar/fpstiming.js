3efa2b5f31489c84e10c546825be64deee5f30a6
Jairo Suarez <jasuarezpr@gmail.com>
2017-05-04 10:47:48 -0500

    implementing interfaces

--- src/Timer.js
+++ src/Timer.js
@@ -1,1 +1,32 @@
-export default class Timer {}
+export default class Timer {
+  constructor() {
+    if (typeof this.run === 'function') {
+      throw new TypeError('run must be overrided');
+    }
+    // TODO: check for timingTask : Taskable
+    if (typeof this.stop === 'function') {
+      throw new TypeError('stop must be overrided');
+    }
+    if (typeof this.cancel === 'function') {
+      throw new TypeError('cancel must be overrided');
+    }
+    if (typeof this.create === 'function') {
+      throw new TypeError('create must be overrided');
+    }
+    if (typeof this.isActive === 'function') {
+      throw new TypeError('isActive must be overrided');
+    }
+    if (typeof this.period === 'number') {
+      throw new TypeError('period must be overrided');
+    }
+    if (typeof this.setPeriod === 'function') {
+      throw new TypeError('setPeriod must be overrided');
+    }
+    if (typeof this.isSingleShot === 'function') {
+      throw new TypeError('isSingleShot must be overrided');
+    }
+    if (typeof this.setSingleShot === 'function') {
+      throw new TypeError('setSingleShot must be overrided');
+    }
+  }
+}
