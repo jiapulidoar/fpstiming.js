b3944114920e30ad6a6107ef551ed54d8cb2a550
Sebastian Chaparro <sechaparroc@unal.edu.co>
2017-11-26 21:46:29 -0500

    Animator and AnimatorObject methods and doc updated

--- src/Animator.js
+++ src/Animator.js
@@ -1,5 +1,5 @@
 /**
- * Interface defining the behavior animated objects should implement.
+ * @interface defining the behavior animated objects should implement.
  */
 export default class Animator {
   /**
@@ -9,71 +9,50 @@ export default class Animator {
     throw new TypeError('animate must be overrided');
   }
   /**
-   * Whether or not the animated method is defined externally, as when register it through reflection.
-   * @returns {boolean}
-   */
-  invokeAnimationHandler() {
-    throw new TypeError('invokeAnimationHandler must be overrided');
-  }
-  /**
    * Returns the animation period in milliseconds.
    * @returns {number}
    */
-  animationPeriod() {
-    throw new TypeError('animationPeriod must be overrided');
+  period() {
+    throw new TypeError('period must be overrided');
   }
   /**
    * Sets the animation `period` in milliseconds and (optional) `restart` the animation.
    * @param {number} period
    * @param {boolean} [restart = false]
    */
-  setAnimationPeriod(period, restart = true) {
-    throw new TypeError('setAnimationPeriod must be overrided');
+  setPeriod(period, restart = true) {
+    throw new TypeError('setPeriod must be overrided');
   }
   /**
    * Stops the animation
    */
-  stopAnimation() {
-    throw new TypeError('stopAnimation must be overrided');
+  stop() {
+    throw new TypeError('stop must be overrided');
   }
   /**
    * Starts the animation executing periodically the animated call back method.
    */
-  startAnimation() {
-    throw new TypeError('startAnimation must be overrided');
+  start() {
+    throw new TypeError('start must be overrided');
   }
   /**
-   * Simply calls {@link Animator#stopAnimation} and then {@link Animator#startAnimation}.
+   * Simply calls {@link Animator#stop} and then {@link Animator#start}.
    */
-  restartAnimation() {
-    throw new TypeError('restartAnimation must be overrided');
+  restart() {
+    throw new TypeError('restart must be overrided');
   }
   /**
-   * Starts or stops the animation according to {@link Animator#animationStarted}.
+   * Starts or stops the animation according to {@link Animator#started}.
    */
-  toggleAnimation() {
-    throw new TypeError('toggleAnimation must be overrided');
+  toggle() {
+    throw new TypeError('toggle must be overrided');
   }
   /**
    * Return `true` if animation was started or `false` otherwise.
    * @returns {boolean}
    */
-  animationStarted() {
-    throw new TypeError('animationStarted must be overrided');
-  }
-  /**
-   * Sets the timing handler.
-   * param {TimingHandler} handler
-   */
-  setTimingHandler(handler) {
-    throw new TypeError('setTimingHandler must be overrided');
-  }
-  /**
-   * Returns the timing handler.
-   * @returns {TimingHandler}
-   */
-  timingHandler() {
-    throw new TypeError('TimingHandler must be overrided');
+  started() {
+    throw new TypeError('started must be overrided');
   }
   /**
    * Returns the sequential timer.
--- src/AnimatorObject.js
+++ src/AnimatorObject.js
@@ -6,82 +6,107 @@ import SeqTimer from './SeqTimer';
  */
 export default class AnimatorObject extends Animator {
   /**
-   * Constructs an animated object with a default {@link AnimatorObject#animationPeriod} of 40 milliseconds (25Hz), if no handler is supplied, the handler should explicitly be defined afterwards {@link AnimatorObject#setTimingHandler}.
-   * @param {TimingHandler} [handler = null] optional {@link TimingHandler}
+   * Constructs an animated object with a default {@link AnimatorObject#animationPeriod} of 40 milliseconds (25Hz)
+   * @param {TimingHandler} handler
    */
-  constructor(handler = null) {
+  constructor(handler) {
     super();
 
     this._animationPeriod = 40;
     this._started = false;
 
     this._animationTimer = null;
-    this._handler = null;
-
-    if (handler !== null) {
-      this.setTimingHandler(handler);
-    }
-  }
-
-  setTimingHandler(handler) {
-    this._animationTimer = new SeqTimer({ handler });
     this._handler = handler;
-    handler.registerAnimator(this);
   }
 
-  timingHandler() {
-    return this._handler;
-  }
-
+  /**
+   * @returns {SeqTimer}
+   */
   timer() {
     return this._animationTimer;
   }
 
-  animationStarted() {
+  /**
+   * Return true when the animation loop is started.
+   * The timing handler will check when {@link AnimatorObject#started} and then called the
+   * animation callback method every {@link AnimatorObject#period} milliseconds.
+   * Use {@link AnimatorObject#start} and {@link AnimatorObject#stop}.
+   * @see AnimatorObject#start
+   * @see AnimatorObject#animate
+   * @returns {boolean}
+   */
+  started() {
     return this._started;
   }
 
-  animationPeriod() {
+  /**
+   * The animation loop period, in milliseconds. When {@link AnimatorObject#started}, this is
+   * the delay that takes place between two consecutive iterations of the animation loop.
+   * This delay defines a target frame rate that will only be achieved if your
+   * {@link AnimatorObject#animate} methods is fast enough.
+   * Default value is 40 milliseconds (25 Hz).
+   * Note: This value is taken into account only the next time you call
+   * {@link AnimatorObject#start}. If {@link AnimatorObject#started}, you should
+   * {@link AnimatorObject#stop} first. See {@link AnimatorObject#restart} and
+   * {@link AnimatorObject#setPeriod}.
+   * @see AnimatorObject#setPeriod
+   * @returns {number}
+   */
+  period() {
     return this._animationPeriod;
   }
 
-  setAnimationPeriod(period, restart = true) {
+  /**
+   * Sets the {@link AnimatorObject#period}, in milliseconds. If restart is true and
+   * {@link AnimatorObject#started} then {@link AnimatorObject#restart} is called.
+   * @see AnimatorObject#start
+   * @param {number} period
+   * @param {boolean} [restart = true]
+   */
+  setPeriod(period, restart = true) {
     if (period > 0) {
       this._animationPeriod = period;
       if (this._started && restart) {
-        this.restartAnimation();
+        this.restart();
       }
     }
   }
 
-  stopAnimation() {
+  /**
+   * Stops animation.
+   * @see AnimatorObject#start
+   */
+  stop() {
     this._started = false;
     if (this._animationTimer != null) {
       this._animationTimer.stop();
     }
   }
 
-  startAnimation() {
+  /**
+   * Starts the animation loop.
+   * @see AnimatorObject#started
+   */
+  start() {
     this._started = true;
     if (this._animationTimer != null) {
       this._animationTimer.run(this._animationPeriod);
     }
   }
 
-  restartAnimation() {
-    this.stopAnimation();
-    this.startAnimation();
+  toggle() {
+      if (this._started) {
+          this.stop();
+      } else {
+          this.start();
+      }
   }
 
-  toggleAnimation() {
-    if (this._started) {
-      this.stopAnimation();
-    } else {
-      this.startAnimation();
-    }
-  }
-
-  invokeAnimationHandler() {
-    return false;
+  /**
+   * Simply calls {@link AnimatorObject#stop} and then {@link AnimatorObject#start}.
+   */
+  restart() {
+    this.stop();
+    this.start();
   }
 }
